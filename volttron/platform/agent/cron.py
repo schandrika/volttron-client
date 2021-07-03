a32c0c9dfcc389bfe0b9cfc006553c3ae052b74e
C. Allwardt <craig.allwardt@pnnl.gov>
2021-07-03 14:48:09 -0700

    removed agents' bases from client

--- volttron/platform/agent/cron.py
+++
@@ -1,4 +0,0 @@
-__import__('warnings').warn(
-    'Module {} is deprecated in favor of volttron.platform.scheduling '
-    'and will be removed in a future version.'.format( __name__),
-    DeprecationWarning)
