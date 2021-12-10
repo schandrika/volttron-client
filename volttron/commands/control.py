7c6e230c18b6b41ba0beb09b459d80e70d242ce5
schandrika <chandrika@pnnl.gov>
2021-12-10 14:57:42 -0800

    fixed vctl tag command

--- volttron/commands/control.py
+++ volttron/commands/control.py
@@ -223,12 +223,12 @@ def tag_agent(opts):
     agent, = agents
     if opts.tag:
         _stdout.write('Tagging {} {}\n'.format(agent.uuid, agent.name))
-        opts.aip.tag_agent(agent.uuid, opts.tag)
+        opts.connection.call('tag_agent', agent.uuid, opts.tag)
     elif opts.remove:
         if agent.tag is not None:
             _stdout.write(
                 'Removing tag for {} {}\n'.format(agent.uuid, agent.name))
-            opts.aip.tag_agent(agent.uuid, None)
+            opts.connection.call('tag_agent', agent.uuid, None)
     else:
         if agent.tag is not None:
             _stdout.writelines([agent.tag, '\n'])
